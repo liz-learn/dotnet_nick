33b2f21b79137aa521b5ffd114c7cfc81e632bd1
Liz Schley <liz.schley@advance-auto.com>
2023-08-15 15:21:15 -0400

    Did update and delete methods also complete but no endpoint for complete

--- TodoApp.Api/Services/DbTodoItemService.cs
+++ TodoApp.Api/Services/DbTodoItemService.cs
@@ -22,27 +22,61 @@ public class DbTodoItemService : ITodoItemService
     {
         var target = _context.TodoItems.SingleOrDefault(x => x.Id == id);
         if (target is null)
-            _logger.LogWarning("No todo fetched: TodoId, {TodoId}, is invalid", id);
+            _logger.LogWarning("No toDo fetched: TodoId, {TodoId}, is invalid", id);
         return target;
     }
 
     public int? Create(string description, bool isCompleted = false)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrWhiteSpace(description))
+        {
+            _logger.LogWarning("Empty descriptions not allowed");
+            return null;
+        }
+
+        var todoItem = new TodoItem{
+            Description = description,
+            CreatedAt = DateTime.UtcNow,
+            IsCompleted = isCompleted
+        };
+        _context.TodoItems.Add(todoItem);
+        _context.SaveChanges();
+        return todoItem.Id;
     }
 
     public void Update(int id, string description, bool isCompleted)
     {
-        throw new NotImplementedException();
+        TodoItem? target = _context.TodoItems.SingleOrDefault(x => x.Id == id);
+        if (target is null)
+        {
+            _logger.LogWarning("Invalid id, {TodoId}, therefore toDo will not be updated", id);
+            return;
+        }
+        target.Description = description;
+        target.IsCompleted = isCompleted;
+        _context.SaveChanges();
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        TodoItem? target = _context.TodoItems.SingleOrDefault(x => x.Id == id);
+        if (target is null) {
+            _logger.LogWarning("Invalid id, {TodoId}, therefore toDo will be deleted", id);
+            return;
+        }
+        _context.TodoItems.Remove(target);
+        _context.SaveChanges();
     }
 
     public void Complete(int id)
     {
-        throw new NotImplementedException();
+        TodoItem? target = _context.TodoItems.SingleOrDefault(x => x.Id == id);
+        if (target is null)
+        {
+            _logger.LogWarning("Invalid id, {TodoId}, therefore toDo will not be completed", id);
+            return;
+        }
+        target.IsCompleted = true;
+        _context.SaveChanges();
     }
 }
